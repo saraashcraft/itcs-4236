7470a2fc2bbe96b80098673aeb89f1e2656a3e9e
Naima <naimakarzouz@gmail.com>
2019-09-29 17:38:11 -0400

    (9/29/2019) Basic Functionality/Base Game
    Shows two separate players, starting the game deals immediately to the player, showing four cards face-up to the player. The hand and deck is randomized.

---
+++ CardGameProto/Assets/Scripts/TriumphLogic.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriumphLogic : MonoBehaviour
+{
+    public Sprite[] cardFaces;
+    public GameObject cardPrefab; 
+
+    public static string[] suits = new string[] { "C", "D", "H", "S" };
+    public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+    public List<string> deck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayCards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void PlayCards() {
+        deck = GenerateDeck();
+        Shuffle(deck); 
+        foreach (string card in deck) {
+            print(card);
+        }
+
+        DealCards();
+    }
+
+    public static List<string> GenerateDeck() {
+        List<string> newDeck = new List<string>();
+        foreach (string s in suits) {
+            foreach (string v in values) {
+                newDeck.Add(s + v);
+            }
+        }
+
+        return newDeck;
+    }
+
+    void Shuffle<T>(List<T> list) {
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while (n > 1) {
+            int k = random.Next(n);
+            n--;
+
+            T temp = list[k];
+            list[k] = list[n];
+            list[n] = temp; 
+        }
+    }
+
+    void DealCards() {
+        //used to offset the dealt cards
+        float xOffset = 6f; 
+        float yOffset = -2.5f;
+
+        Vector3 originalPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        int drawFour = 0; //if <4, put onto player's hand, otherwise, stay in deck. 
+
+
+        foreach (string card in deck) {
+            //this makes it in a row. 
+            GameObject newCard;
+            
+
+            if (drawFour < 4)
+            {
+                newCard = Instantiate(cardPrefab, new Vector3(transform.position.x + xOffset, transform.position.y - yOffset, transform.position.z), Quaternion.identity);
+                newCard.name = card;
+
+
+                newCard.GetComponent<Select>().faceUp = true;
+                xOffset += 2f;
+                drawFour++;
+            }
+            else {
+                xOffset = 0f;
+                yOffset = -2.5f;
+
+                newCard = Instantiate(cardPrefab, new Vector3(transform.position.x + xOffset, transform.position.y - yOffset, transform.position.z), Quaternion.identity);
+                newCard.name = card;
+
+                
+                newCard.GetComponent<Select>().faceUp = false;
+            }
+        }
+    }
+}
---
+++ CardGameProto/Assets/Scripts/UpdateSprite.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateSprite : MonoBehaviour
+{
+    public Sprite cardFace;
+    public Sprite cardBack;
+
+    private SpriteRenderer spriteRenderer;
+    private Select select;
+    private TriumphLogic triumphLogic;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> deck = TriumphLogic.GenerateDeck();
+        triumphLogic = FindObjectOfType<TriumphLogic>();
+
+        int i = 0;
+        foreach (string card in deck) {
+            if (this.name == card) {
+                cardFace = triumphLogic.cardFaces[i];
+                break; 
+            }
+            i++; 
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        select = GetComponent<Select>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (select.faceUp == true)
+        {
+            spriteRenderer.sprite = cardFace;
+        }
+        else {
+            spriteRenderer.sprite = cardBack; 
+        }
+    }
+}
