17c917ac70b640d17e5ee2b6a888fc7a445ffcff
sashcra1 <saraxashcraft@gmail.com>
2019-11-25 23:24:25 -0500

    Main Menu + Rules Scenes
    Added functioning main menu and rules page. Next steps include adding music and tweaking certain values like length of time opponent's cards are shown.

---
+++ CardGameProto/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /*
+     * Main menu = index 0
+     * Game = index 1
+     * Rules = index 2
+     */
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ViewRules()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ CardGameProto/Assets/Scripts/RulesMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RulesMenu : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
